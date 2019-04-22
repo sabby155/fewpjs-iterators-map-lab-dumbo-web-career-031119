b6f2754fc73f7eff4612b2238e8f86f64f23c4b6
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-22 18:27:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  tutorials.split(' ');
-  return tutorials.map(x => x.charAt(0).toUpperCase() + x.slice(1));
+
+  return tutorials.map(x => x.split; x.charAt(0).toUpperCase() + x.slice(1));
 }
 
